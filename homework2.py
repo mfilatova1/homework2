03d873a0903559b6bccca250d3e5e83d92a7567d
mfilatova1 <filatovam435@gmail.com>
2024-06-08 16:33:51 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+completed_homework = 12
+total_hours = 1.5
+course_name = "Python"
+time_for_1task = (total_hours / completed_homework)
+print("Курс:",course_name,", всего задач:", completed_homework , ", затрачено часов:", total_hours ,", среднее время выполнения:", time_for_1task)
+
+
+
